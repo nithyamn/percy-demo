dc89428ff42d2d20bb9955cff71e5d8c4d44d8cb
nithyamani <nithya.mn999@gmail.com>
2021-10-22 16:18:01 +0530

    for gitlab

--- src/test/java/PercyTest.java
+++ src/test/java/PercyTest.java
@@ -9,7 +9,7 @@ public class PercyTest {
     @Parameters({"test_name","url"})
     @Test
     public void test(String test_name, String url) throws Exception {
-        System.setProperty("webdriver.chrome.driver", "/Users/nithyamani/Desktop/chromedrivers/chromedriver91");
+        System.setProperty("webdriver.chrome.driver", "/Users/nithyamani/Desktop/Tools/chromedrivers/chromedriver94");
         WebDriver driver = new ChromeDriver();
         Percy percySnapshots = new Percy(driver);
         driver.get(url);
@@ -17,6 +17,7 @@ public class PercyTest {
 
         driver.findElement(By.linkText("Contact Us!")).click();
         percySnapshots.snapshot("Contact Us Page "+test_name);
+        percySnapshots.snapshot("contact_us.png");
 
         Thread.sleep(2000);
         driver.quit();
