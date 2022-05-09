4a8d428c74bf72b1533374c38f96a3c2bede38f5
nithyamani <nithya.mn999@gmail.com>
2022-05-09 15:54:15 +0530

    yml updated

--- src/test/java/PercyTest.java
+++ src/test/java/PercyTest.java
@@ -9,15 +9,15 @@ public class PercyTest {
     @Parameters({"test_name","url"})
     @Test
     public void test(String test_name, String url) throws Exception {
-        System.setProperty("webdriver.chrome.driver", "/Users/nithyamani/Desktop/Tools/chromedrivers/chromedriver96");
+        System.setProperty("webdriver.chrome.driver", "/Users/nithyamani/Desktop/Tools/chromedrivers/chromedriver100");
         WebDriver driver = new ChromeDriver();
         Percy percySnapshots = new Percy(driver);
         driver.get(url);
         percySnapshots.snapshot("Main Page "+test_name);
+        driver.findElement(By.linkText("Contact Us!")).click();
 
         driver.findElement(By.linkText("Contact Us!")).click();
         percySnapshots.snapshot("Contact Us Page "+test_name);
-        percySnapshots.snapshot("contact_us.png");
 
         Thread.sleep(2000);
         driver.quit();
