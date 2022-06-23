bc18e5768d94b6f609afc35243d9c84b2b216734
nithyamani <nithya.mn999@gmail.com>
2022-06-23 12:34:51 +0530

    updates to feature branch

--- src/test/java/PercyTest.java
+++ src/test/java/PercyTest.java
@@ -5,20 +5,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class PercyTest {
     @Parameters({"test_name","url"})
     @Test
     public void test(String test_name, String url) throws Exception {
-        System.setProperty("webdriver.chrome.driver", "/Users/nithyamani/Desktop/Tools/chromedrivers/chromedriver100");
+        System.setProperty("webdriver.chrome.driver", "/Users/nithyamani/Desktop/Tools/chromedrivers/chromedriver102");
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
         Percy percySnapshots = new Percy(driver);
+
         driver.get(url);
         percySnapshots.snapshot("Main Page "+test_name);
 
         driver.findElement(By.linkText("Contact Us!")).click();
         percySnapshots.snapshot("Contact Us Page "+test_name);
-
-        driver.findElement(By.cssSelector("button[data-button-id='button1']")).getAttribute("value");
 
         Thread.sleep(2000);
         driver.quit();
